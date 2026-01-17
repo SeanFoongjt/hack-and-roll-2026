9865917e7547c0f2c86c40eea786cabe38780989
SeanFoongjt <seanfoongjt@gmail.com>
2026-01-17 16:54:46 +0800

    Fix ES Modules shenanigans

--- peptalk_buddy/eslint.config.js
+++ peptalk_buddy/eslint.config.js
@@ -1,10 +1,9 @@
 // https://docs.expo.dev/guides/using-eslint/
-import { defineConfig } from "eslint/config";
-import expoConfig from "eslint-config-expo/flat.js";
+const expoConfig = require("eslint-config-expo/flat.js");
 
-export default defineConfig([
+module.exports = [
   expoConfig,
   {
     ignores: ["dist/*"],
   },
-]);
+];
